8879d370270eb30f28d30905caf8f50cb21edf8b
harlo <harlo.holmes@gmail.com>
2016-04-30 08:36:57 -0400

    more pseudocode for ya

--- pseudocode.js
+++ pseudocode.js
@@ -1,15 +1,51 @@
-// anytime you see "%s", it is supposed to be a string formatting operation!
-// javascript does not natively support it, but templating engines exist (i.e. in Backbone?)
-// you may always brute-force it, ie: 
-// var template = "?lat=" + lat + "&long=" + lon
+/*
+ anytime you see "<%= param_name %>", it is supposed to be a string formatting operation!
+ javascript does not natively support it, but templating engines exist.
 
-var osm_lookup = "http://nominatim.openstreetmap.org/search?format=json&limit=1&q=%s";
-var sunlight_lookup = "http://openstates.org/api/v1/legislators/geo/?lat=%s&long=%s";
+ for this example, I'm using backbone because i loooooooove it (backbonejs.org)
+ I'm also using jQuery to handle the ajax request
+
+ but you may always brute-force it, ie: 
+ var template = "?lat=" + lat + "&long=" + lon
+*/
+
+var osm_lookup = "http://nominatim.openstreetmap.org/search?format=json&limit=1&q=<%= address %>";
+var sunlight_lookup = "http://openstates.org/api/v1/legislators/geo/?lat=<%= latitude %>&long=<%= longitude %>&apikey=<%= apikey %>";
+
+function buildResultForUser() {
+	// THIS IS YOUR PRIMARY FUNCTION!
+	// How do you combine the data from our two APIs together to show a result for the user?
+}
+
+function doAjaxSyncronousRequest(url_template, params) {
+	return $.ajax({
+		async : false,
+		dataType : "json",
+		url : _.template(url_template, params)
+	});
+}
 
 function getLatLonFromAddress(address) {
 	// use OpenStreetMaps API
+	// OSM requires spaces to be replaced by '+'
+	
+	var params = {
+		address : address.replace(\ \g, '+')
+	};
+
+	return doAjaxSyncronousRequest(osm_lookup, params);
+
 }
 
-function getLegislatorFromLatLong(lat, long) {
+function getLegislatorFromLatLong(latitude, longitude) {
 	// use Sunlight FDN API
+	var apikey = "ABCDEFG!";
+
+	var params = {
+		latitude : latitude, 
+		longitude : longitude,
+		apikey : apikey
+	};
+
+	return doAjaxSyncronousRequest(sunlight_lookup, params);
 }
